ff65a0dd6121b6bb35c533c43c950873d671fc33
FranciaMaria <mariafrancia88@gmail.com>
2017-10-19 20:31:52 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { MoviesComponent } from './components/movies/movies.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    component: MoviesComponent,
+    /*children: [
+      {
+        path: ':id',
+        component: ContactDetailsComponent
+      }
+    ]*/
+  },
+ /* {
+    path: 'messages',
+    component: MessagesComponent
+  },*/
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    MoviesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+	)
+  ],
+  exports: [
+  	LayoutComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
